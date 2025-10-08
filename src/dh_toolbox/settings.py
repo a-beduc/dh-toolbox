d97f95be0cb7d53a5a724bcba052747840e77e76
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-10-08 06:27:52 +0200

    structure: fix

--- src/dh_toolbox/settings.py
+++ src/dh_toolbox/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -76,7 +75,7 @@ WSGI_APPLICATION = 'dh_toolbox.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR.parent / 'db.sqlite3',
     }
 }
 
