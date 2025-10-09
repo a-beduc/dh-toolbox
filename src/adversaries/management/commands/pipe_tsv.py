b4b6dfbb8cfd10b1c350efdc5db5bce313a44fd5
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-10-09 23:17:38 +0200

    feature(incomplete): new custom commands to populate db with data from .tsv

---
+++ src/adversaries/management/commands/pipe_tsv.py
@@ -0,0 +1,30 @@
+from importlib.util import spec_from_file_location, module_from_spec
+from pathlib import Path
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+def import_script_from_path(scriptpath):
+    spec = spec_from_file_location("script_parser", scriptpath)
+    mod = module_from_spec(spec)
+    spec.loader.exec_module(mod)
+    return mod
+
+
+class Command(BaseCommand):
+    help = "Transform tsv input into db entries"
+    default_scriptpath = (settings.BASE_DIR / "adversaries" / "scripts" /
+                          "tsv_parser.py")
+
+    def add_arguments(self, parser):
+        parser.add_argument("tsv_filepath")
+        parser.add_argument('-s', '--script_filepath')
+
+    def handle(self, *args, **options):
+        filepath = Path(options["tsv_filepath"]).resolve()
+        scriptpath = Path(options["script_filepath"]).resolve() \
+            if options["script_filepath"] \
+            else self.default_scriptpath
+        mod = import_script_from_path(scriptpath)
+        print(mod.parse_tsv(filepath))
