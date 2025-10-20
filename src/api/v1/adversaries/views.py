c6a2ab67a1b96e789585afa5d47cb056cfb22232
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-10-20 23:48:50 +0200

    Feature: Views and routing for Adversary endpoint

--- src/api/v1/adversaries/views.py
+++ src/api/v1/adversaries/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from adversaries.models import Adversary
+from api.v1.adversaries.serializers import AdversaryReadSerializer, \
+    AdversaryWriteSerializer, AdversaryListSerializer
+
+
+class AdversaryViewSet(ModelViewSet):
+    queryset = Adversary.objects.all()
+    serializer_class = AdversaryWriteSerializer
+
+    action_serializer_classes = {
+        "create": AdversaryWriteSerializer,
+        "update": AdversaryWriteSerializer,
+        "retrieve": AdversaryReadSerializer,
+        "list": AdversaryListSerializer,
+        "partial_update": AdversaryWriteSerializer,
+    }
+
+    def get_serializer_class(self):
+        try:
+            return self.action_serializer_classes[self.action]
+        except (KeyError, AttributeError):
+            return super().get_serializer_class()
+
+    def get_queryset(self):
+        return (
+            Adversary.objects
+            .select_related("author", "basic_attack__damage")
+            .prefetch_related("tactics", "tags", "features",
+                              "adversary_experiences__experience")
+        )
--- src/api/v1/urls.py
+++ src/api/v1/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from api.v1.adversaries.views import AdversaryViewSet
 from api.v1.lookups.views import ExperienceViewSet, TacticViewSet, \
     FeatureViewSet, TagViewSet
 
@@ -14,6 +15,8 @@ router.register(r"lookups/features", FeatureViewSet,
                 basename="lookup-features")
 router.register(r"lookups/tags", TagViewSet,
                 basename="lookup-tags")
+router.register(r"adversaries", AdversaryViewSet,
+                basename="adversaries")
 
 
 urlpatterns = [
