89034caec865559f51c5677a2540c9791298f5d9
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-10-30 03:47:35 +0100

    Refactoring: manual lookups endpoint (list/detail)

--- src/api/v1/lookups/views.py
+++ src/api/v1/lookups/views.py
@@ -1,25 +1,98 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from adversaries.models import Experience, Tactic, Feature, Tag
-from api.v1.lookups.serializers import ExperienceReadSerializer, \
-    TacticReadSerializer, FeatureReadSerializer, TagReadSerializer
-
-
-class ExperienceViewSet(ReadOnlyModelViewSet):
-    queryset = Experience.objects.all()
-    serializer_class = ExperienceReadSerializer
+from adversaries.selectors import experience_list, experience_get, \
+    tactic_list, tactic_get, tag_list, tag_get, feature_list, feature_get
 
 
-class TacticViewSet(ReadOnlyModelViewSet):
-    queryset = Tactic.objects.all()
-    serializer_class = TacticReadSerializer
+class ExperienceCollectionApi(APIView):
+    class OutputSerializer(serializers.Serializer):
+        id = serializers.IntegerField()
+        name = serializers.CharField()
+
+    def get(self, request):
+        experiences = experience_list()
+        data = self.OutputSerializer(experiences, many=True).data
+        return Response(data)
 
 
-class FeatureViewSet(ReadOnlyModelViewSet):
-    queryset = Feature.objects.all()
-    serializer_class = FeatureReadSerializer
+class ExperienceItemApi(APIView):
+    class OutputSerializer(serializers.Serializer):
+        id = serializers.IntegerField()
+        name = serializers.CharField()
+
+    def get(self, request, experience_id):
+        experiences = experience_get(pk=experience_id)
+        data = self.OutputSerializer(experiences).data
+        return Response(data)
 
 
-class TagViewSet(ReadOnlyModelViewSet):
-    queryset = Tag.objects.all()
-    serializer_class = TagReadSerializer
+class FeatureCollectionApi(APIView):
+    class OutputSerializer(serializers.Serializer):
+        id = serializers.IntegerField()
+        name = serializers.CharField()
+        type = serializers.CharField()
+        description = serializers.CharField()
+
+    def get(self, request):
+        features = feature_list()
+        data = self.OutputSerializer(features, many=True).data
+        return Response(data)
+
+
+class FeatureItemApi(APIView):
+    class OutputSerializer(serializers.Serializer):
+        id = serializers.IntegerField()
+        name = serializers.CharField()
+        type = serializers.CharField()
+        description = serializers.CharField()
+
+    def get(self, request, feature_id):
+        features = feature_get(pk=feature_id)
+        data = self.OutputSerializer(features).data
+        return Response(data)
+
+
+class TacticCollectionApi(APIView):
+    class OutputSerializer(serializers.Serializer):
+        id = serializers.IntegerField()
+        name = serializers.CharField()
+
+    def get(self, request):
+        tactics = tactic_list()
+        data = self.OutputSerializer(tactics, many=True).data
+        return Response(data)
+
+
+class TacticItemApi(APIView):
+    class OutputSerializer(serializers.Serializer):
+        id = serializers.IntegerField()
+        name = serializers.CharField()
+
+    def get(self, request, tactic_id):
+        tactic = tactic_get(pk=tactic_id)
+        data = self.OutputSerializer(tactic).data
+        return Response(data)
+
+
+class TagCollectionApi(APIView):
+    class OutputSerializer(serializers.Serializer):
+        id = serializers.IntegerField()
+        name = serializers.CharField()
+
+    def get(self, request):
+        tags = tag_list()
+        data = self.OutputSerializer(tags, many=True).data
+        return Response(data)
+
+
+class TagItemApi(APIView):
+    class OutputSerializer(serializers.Serializer):
+        id = serializers.IntegerField()
+        name = serializers.CharField()
+
+    def get(self, request, tag_id):
+        tag = tag_get(pk=tag_id)
+        data = self.OutputSerializer(tag).data
+        return Response(data)
