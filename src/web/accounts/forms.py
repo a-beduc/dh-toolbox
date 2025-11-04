686ebc8eb3e4704b8b4b29c44e245dd1f0758ba1
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-11-04 23:02:46 +0100

    Feature: login/logout/register view

---
+++ src/web/accounts/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    email = forms.EmailField()
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,9 @@ from accounts.models import Account
 @pytest.fixture
 def conf_account():
     return Account.objects.create(username="lou")
+
+
+@pytest.fixture
+def fast_auth(settings):
+    settings.PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+    settings.AUTH_PASSWORD_VALIDATORS = []
